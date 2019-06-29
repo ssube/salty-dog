cd3be38db9dc05ba4c50990269f4ae81a75abf10
ssube <seansube@gmail.com>
2019-06-29 11:39:49 -0500

    fix(scripts): use project path for status

--- scripts/github-status.sh
+++ scripts/github-status.sh
@@ -8,4 +8,4 @@ STATUS_BODY="{\"state\": \"${STATUS}\", \"target_url\": \"${CI_PIPELINE_URL}\", 
 printf "Reporting status for %s...\n%s" "${CI_COMMIT_SHA}" "${STATUS_BODY}"
 printf "%s" "${STATUS_BODY}" | curl -d @- \
   -H "Authorization: token $(printf "%s" "${GITHUB_SECRET}" | base64 -d)" \
-  -i "https://api.github.com/repos/ssube/salty-dog/statuses/${CI_COMMIT_SHA}"
+  -i "https://api.github.com/repos/${CI_PROJECT_PATH}/statuses/${CI_COMMIT_SHA}"
