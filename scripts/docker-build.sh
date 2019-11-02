94fafb7fb77918cd18a3c0706ac6776859410204
ssube <seansube@gmail.com>
2019-11-01 20:04:55 -0500

    fix typo

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -6,7 +6,7 @@ IMAGE_DEFAULT="${2:---skip}"
 IMAGE_NAME="${CI_PROJECT_PATH}"
 IMAGE_TAG="$(echo "${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}" | sed -r 's/[^-_a-zA-Z0-9\\.]/-/g')"
 
-IAMGE_SHORT="${IMAGE_NAME}:${IMAGE_TAG}"
+IMAGE_SHORT="${IMAGE_NAME}:${IMAGE_TAG}"
 IMAGE_FULL="${IMAGE_NAME}:${IMAGE_TAG}-${IMAGE_ARCH}"
 
 echo "Building image: ${IMAGE_FULL}"
