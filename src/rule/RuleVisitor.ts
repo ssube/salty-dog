b23bdd9c0f6789c653497fe841cbd4bb8e3a52d2
ssube <seansube@gmail.com>
2019-11-19 08:23:10 -0600

    feat(visitor): emit item visit, diff, error, and pass events

--- src/rule/RuleVisitor.ts
+++ src/rule/RuleVisitor.ts
@@ -14,9 +14,13 @@ export interface RuleVisitorOptions {
 }
 
 export enum RuleVisitorEvents {
-  RULE_VISIT = 'rule-visit',
+  ITEM_DIFF = 'item-diff',
+  ITEM_ERROR = 'item-error',
+  ITEM_PASS = 'item-pass',
+  ITEM_VISIT = 'item-visit',
   RULE_ERROR = 'rule-error',
   RULE_PASS = 'rule-pass',
+  RULE_VISIT = 'rule-visit',
 }
 
 export class RuleVisitor extends EventEmitter implements RuleVisitorOptions, Visitor {
@@ -38,6 +42,8 @@ export class RuleVisitor extends EventEmitter implements RuleVisitorOptions, Vis
         rule,
       });
 
+      const errorsBefore = ctx.errors.length;
+
       const items = await rule.pick(ctx, root);
       let itemIndex = 0;
       for (const item of items) {
@@ -49,42 +55,62 @@ export class RuleVisitor extends EventEmitter implements RuleVisitorOptions, Vis
         await this.visitItem(ctx, item, itemIndex, rule);
         itemIndex += 1;
       }
+
+      if (ctx.errors.length > errorsBefore) {
+        ctx.logger.info({ rule: rule.name }, 'rule failed');
+        this.emit(RuleVisitorEvents.RULE_ERROR, {
+          rule,
+        });
+      } else {
+        ctx.logger.info({ rule: rule.name }, 'rule passed');
+        this.emit(RuleVisitorEvents.RULE_PASS, {
+          rule,
+        });
+      }
     }
 
     return ctx;
   }
 
-  public async visitItem(ctx: VisitorContext, item: any, itemIndex: number, rule: Rule): Promise<void> {
+  public async visitItem(ctx: VisitorContext, item: any, itemIndex: number, rule: Rule): Promise<VisitorContext> {
     const itemResult = cloneDeep(item);
     const ruleResult = await rule.visit(ctx, itemResult);
 
     if (hasItems(ruleResult.errors)) {
-      ctx.logger.warn({ count: ruleResult.errors.length, rule }, 'rule failed');
-      ctx.mergeResult(ruleResult, ctx.visitData);
-      this.emit(RuleVisitorEvents.RULE_ERROR, {
+      const errorData = {
+        count: ruleResult.errors.length,
         item,
         rule,
-      });
-      return;
+      };
+      ctx.logger.warn(errorData, 'item failed');
+      this.emit(RuleVisitorEvents.ITEM_ERROR, errorData);
+
+      ctx.mergeResult(ruleResult, ctx.visitData);
+      return ctx;
     }
 
     const itemDiff = diff(item, itemResult);
     if (hasItems(itemDiff)) {
-      ctx.logger.info({
+      const diffData = {
         diff: itemDiff,
         item,
-        rule: rule.name,
-      }, 'rule passed with modifications');
+        rule,
+      };
+      ctx.logger.info(diffData, 'item could pass rule with changes');
+      this.emit(RuleVisitorEvents.ITEM_DIFF, diffData);
 
       if (ctx.schemaOptions.mutate) {
         applyDiff(item, itemResult);
       }
-    } else {
-      ctx.logger.info({ rule: rule.name }, 'rule passed');
-      this.emit(RuleVisitorEvents.RULE_PASS, {
-        item,
-        rule,
-      });
     }
+
+    const passData = {
+      item,
+      rule,
+    };
+    ctx.logger.debug(passData, 'item passed');
+    this.emit(RuleVisitorEvents.ITEM_PASS, passData);
+
+    return ctx;
   }
 }
