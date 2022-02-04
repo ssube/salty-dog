b6a515d08e19057eb247a53b59783e4027d6591e
Sean Sube <seansube@gmail.com>
2022-02-04 08:32:09 -0600

    fix(build): restore codeclimate target

--- run.mk
+++ run.mk
@@ -47,3 +47,7 @@ full: ## ultra thorough build (looong)
 	$(MAKE) clean-target local-stretch local-chown-leaks
 	$(MAKE) clean-target
 	@echo "Full build (CI, alpine, stretch) succeeded!"
+
+upload-climate:
+	cc-test-reporter format-coverage -t lcov -o $(TARGET_PATH)/coverage/codeclimate.json -p $(ROOT_PATH) $(TARGET_PATH)/coverage/lcov.info
+	cc-test-reporter upload-coverage --debug -i $(TARGET_PATH)/coverage/codeclimate.json -r "$(shell echo "${CODECLIMATE_SECRET}" | base64 -d)"
